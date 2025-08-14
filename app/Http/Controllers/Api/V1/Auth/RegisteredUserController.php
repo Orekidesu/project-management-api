5a3d41be1bf3a8ddaf23a6c54aa29c2ce1eab4ed
Orekidesu <cmcodog10@gmail.com>
2025-08-15 01:16:32 +0800

    Fix registration request validation and response structure in RegisteredUserController

--- app/Http/Controllers/Api/V1/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisteredUserController.php
@@ -21,13 +21,15 @@ class RegisteredUserController extends Controller
     public function store(Request $request): Response
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
@@ -36,6 +38,14 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return response()->noContent();
+        return response(
+            [
+                'message' => 'user created successfully',
+                'user' => $user,
+                'token' => $user->createToken('api-token')->plainTextToken,
+
+            ],
+            201
+        );
     }
 }
